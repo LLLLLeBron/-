76151717d639597dd736ac3cd5abd8d062f7ac9f
LLLLLeBron <58382057+LLLLLeBron@users.noreply.github.com>
2021-03-01 16:35:45 +0800

    Add files via upload

--- 221801331/src/WordCount.java
+++ 221801331/src/WordCount.java
@@ -5,7 +5,6 @@ public class WordCount
 {
   public static void main(String[] args) throws IOException
   {
-
     FileParser fileParser=new FileParser(new File(args[0]),args[1]);
     fileParser.countValidChars();
     fileParser.countValidLines();
